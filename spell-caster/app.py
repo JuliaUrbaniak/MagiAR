6e11ad3f2ea4575f38e14269c8c4d05f95627326
Jakub <245762@edu.p.lodz.pl>
2025-05-23 11:06:01 +0200

    Added port for Flask

--- spell-caster/app.py
+++ spell-caster/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import base64
@@ -29,4 +31,5 @@ def zaklecie():
     return jsonify({"zaklecie": "Expelliarmus"})  # Przykładowa odpowiedź
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
